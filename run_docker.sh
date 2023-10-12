34cf3fe9ecab5c70dd26cfd9a7127f3d2dded786
dineshsadras <122027492+dineshsadras@users.noreply.github.com>
2023-10-12 09:18:50 +0530

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=nginx .
+docker build --tag=story-generator .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run Nginx hello world app
-docker run -p 8080:80 nginx
+docker run -p 8080:80 story-generator
