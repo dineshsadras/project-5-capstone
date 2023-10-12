8e7358c8b598d63f4d26effe38a0b0d72e9c446f
dineshsadras <122027492+dineshsadras@users.noreply.github.com>
2023-10-12 08:53:56 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Dinesh Kumar "+ Fore.YELLOW+ Back.BLUE+"This is project capstone")
+print(Back.CYAN+"Deploying simple python application")
+print(Fore.RED + Back.GREEN+ "Welcome !!")
