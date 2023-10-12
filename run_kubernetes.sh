0ecce43253408e68818b44cb17af7fa97a099126
dineshsadras <122027492+dineshsadras@users.noreply.github.com>
2023-10-12 09:19:50 +0530

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,11 +4,11 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath=vinusankar/nginx
+dockerpath=vinusankar/story-generator
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run nginx \
+kubectl run story-generator \
     --image=$dockerpath \
     --image-pull-policy="Always" \
     --overrides='{"apiVersion": "v1", "spec":{"imagePullSecrets": [{"name": "regcred"}]}}'
