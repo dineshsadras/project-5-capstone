73bee2f5a4eb82d3d81a7f1a718b902fe2dc2226
dineshsadras <122027492+dineshsadras@users.noreply.github.com>
2023-10-14 00:40:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-import random
-when = ['A few years ago', 'Yesterday', 'Last night', 'A long time ago','On 20th Jan']
-who = ['a rabbit', 'an elephant', 'a mouse', 'a turtle','a cat']
-name = ['Ali', 'Miriam','daniel', 'Hoouk', 'Starwalker']
-residence = ['Barcelona','India', 'Germany', 'Venice', 'England']
-went = ['cinema', 'university','seminar', 'school', 'laundry']
-happened = ['made a lot of friends','Eats a burger', 'found a secret key', 'solved a mistery', 'wrote a book']
-print(random.choice(when) + ', ' + random.choice(who) + ' that lived in ' + random.choice(residence) + ', went to the ' + random.choice(went) + ' and ' + random.choice(happened))
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World, My name is Dinesh'
+
+app.run(host='0.0.0.0', port=80)
