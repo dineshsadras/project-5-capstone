a1348e6f8c5e0c829f9f3311fad62c6e7dc8a35b
dineshsadras <122027492+dineshsadras@users.noreply.github.com>
2023-10-14 16:34:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello World, My name is Dinesh Kumar SG. This is for Udacity project 5 - Capstone'
-failing lint
 
 app.run(host='0.0.0.0', port=80)
 
